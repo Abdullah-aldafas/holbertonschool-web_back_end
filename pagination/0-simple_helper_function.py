c33ab31147819e8fb8d2dbc4fd023412d136b249
Abdullah <Assf23134@icloud.com>
2025-10-24 17:39:40 +0000

    Solved Task 0 And Add Readme AND Add popular_baby_name

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function named index_range that takes two integer arguments page and page_size """
+
+def index_range(page ,page_size):
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start,end)
