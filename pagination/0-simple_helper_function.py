9833c1d862dac249b115451356d9dbffd0c4203a
Abdullah <Assf23134@icloud.com>
2025-10-24 17:48:14 +0000

    Solved Task 0 And Add Readme AND Add popular_baby_name

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,7 +2,8 @@
 """ Range simple helper fun """
 
 def index_range(page: int ,page_size: int) -> tuple[int, int]:
-    """Return a tuple containing the start and end indexes for pagination based on the given page and page_size."""
+    """Return a tuple containing the start and end indexes 
+    for pagination based on the given page and page_size."""
 
     start = (page - 1) * page_size
     end = page * page_size
