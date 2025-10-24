0dc66d212153b4367c78caba2313b3165c0fb2f6
Abdullah <Assf23134@icloud.com>
2025-10-24 17:47:26 +0000

    Solved Task 0 And Add Readme AND Add popular_baby_name

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """ Range simple helper fun """
 
-def index_range(page ,page_size):
-    """ return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters """
-    
+def index_range(page: int ,page_size: int) -> tuple[int, int]:
+    """Return a tuple containing the start and end indexes for pagination based on the given page and page_size."""
+
     start = (page - 1) * page_size
     end = page * page_size
     return (start,end)
