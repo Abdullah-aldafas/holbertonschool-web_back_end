d0617dba1c673e8998fcf5759c36108771c09679
Abdullah <Assf23134@icloud.com>
2025-10-24 18:34:09 +0000

    solved Task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python3
+"""
+Simple Pagination
+"""
 
 import csv
 import math
 from typing import List
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
-    """return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters"""
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
-
-
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -33,13 +28,19 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the requested page of the dataset."""
-        assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0
+        """ Get Page """
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
 
-        start, end = index_range(page, page_size)
+        def index_range(page: int, page_size: int) -> tuple:
+            """ index """
+            return (page - 1) * page_size, page * page_size
+        items = index_range(page, page_size)
+        start = items[0]
+        end = items[1]
+        get_dataset = self.dataset()
+        get_page = get_dataset[start: end]
 
-        data = self.dataset()
-        if start >= len(data):
-            return []
-        return data[start:end]
+        return get_page
