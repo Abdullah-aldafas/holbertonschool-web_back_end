a8ea055ceaca1140a90ca32903f7343a66b7245f
Abdullah <Assf23134@icloud.com>
2025-10-24 18:30:26 +0000

    solved Task 3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -4,11 +4,13 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
+import math
 from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -16,42 +18,44 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """ Cached dataset """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0"""
+        """ Dataset indexed by sorting position, starting at 0 """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
-                i: truncated_dataset[i] for i in range(len(truncated_dataset))
+                i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
-        """Return a deletion-resilient hypermedia pagination dictionary."""
-        dataset = self.indexed_dataset()
-        assert isinstance(index, int) and index >= 0
-        assert index < len(dataset)
-        assert isinstance(page_size, int) and page_size > 0
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ Get Hyper Index """
+        assert isinstance(index, int)
+        assert isinstance(page_size, int)
 
-        data = []
-        current_index = index
+        assert page_size > 0
+        assert 0 <= index < len(self.indexed_dataset())
 
-        while len(data) < page_size and current_index < len(dataset):
-            if current_index in dataset:
-                data.append(dataset[current_index])
-            current_index += 1
-
-        return {
-            "index": index,
-            "data": data,
-            "page_size": len(data),
-            "next_index": current_index
-        }
+        my_list, my_dict = list(), dict()
+        next_index = index + page_size
+        index_dataset = self.indexed_dataset()
+        for i in range(index, next_index):
+            if index_dataset.get(i):
+                my_list.append(index_dataset[i])
+            else:
+                next_index += 1
+                i += 1
+        my_dict['data'] = my_list
+        my_dict['index'] = index
+        my_dict['next_index'] = next_index
+        my_dict['page_size'] = page_size
+        return my_dict
