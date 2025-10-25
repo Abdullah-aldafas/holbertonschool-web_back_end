3f6a245c337a47ae094ac2cea55942cef6296283
Abdullah <Assf23134@icloud.com>
2025-10-25 03:08:02 +0000

    Solved Task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    # Create a MongoDB client
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    # Select the 'logs' database then the 'nginx' collection
+    col = client.logs.nginx
+
+    print("{} logs".format(col.count_documents({})))
+    print("Methods:")
+    print("\tmethod GET: {}".format(col.count_documents({'method': 'GET'})))
+    print("\tmethod POST: {}".format(col.count_documents({'method': 'POST'})))
+    print("\tmethod PUT: {}".format(col.count_documents({'method': 'PUT'})))
+    print("\tmethod PATCH: {}".format(col.count_documents({'method': 'PATCH'})))
+    print("\tmethod DELETE: {}".format(col.count_documents({'method': 'DELETE'})))
+    print("{} status check".format(col.count_documents({'method': 'GET', 'path': '/status'})))
