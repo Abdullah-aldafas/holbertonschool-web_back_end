020a72b5ed97050a8a507e8a84e0919ed3d2d60b
Abdullah <Assf23134@icloud.com>
2025-10-25 02:55:26 +0000

    Solved Task 8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,6 +4,10 @@
 
 
 def list_all(mongo_collection):
+    """
+    Return a list of all documents in the collection.
+    If the collection is empty (or None), return an empty list.
+    """
     if mongo_collection is None:
         return[]
     return list(mongo_collection.find())
