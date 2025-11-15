dc3e307005357441d6afc43c59a33e099441c61c
Abdullah-aldafas <138772608+Abdullah-aldafas@users.noreply.github.com>
2025-11-15 18:28:00 +0300

    Update 5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,25 +1,36 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const filePath = process.argv[2];
+const DATABASE = args[0];
 
-const promise = countStudents(filePath);
+const hostname = '127.0.0.1';
+const port = 1245;
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/students') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    promise.then((data) => {
-      res.end(`This is the list of our students\n${data}`);
-    }).catch(() => { res.end('This is the list of our students\nCannot load the database'); });
-  } else if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-module.exports = app.listen(port, hostname);
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
